a5fda0ba43fb967eaa85694539c442af3bcbbd50
Bruno Albuquerque Carneiro <bruno.carneiro@wisereducacao.com>
2022-08-27 21:07:13 -0300

    ...

--- util/check-environment.ts
+++ util/check-environment.ts
@@ -1,8 +1,5 @@
 export default function checkEnvironment(): string {
-  const envUrl =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : 'https://trello-clone-one.vercel.app';
+  const envUrl = 'https://trello-clone-one.vercel.app';
 
   return envUrl;
 }
